b1aa377028ab9f3f830fef008648053d35b4373f
Andrey Derkach <andrey.derkach.cr@gmail.com>
2021-09-13 11:30:29 +0300

    add loader

--- cere_sdk/src/main/java/io/cere/cere_sdk/WebviewActivity.kt
+++ cere_sdk/src/main/java/io/cere/cere_sdk/WebviewActivity.kt
@@ -17,6 +17,9 @@ class WebviewActivity : AppCompatActivity(), PageLoadingListener {
 
     private lateinit var webview: WebView
     private var lawProgress: LottieAnimationView? = null
+    private var flContent: FrameLayout? = null
+    private var needBlockWebView = false
+    private val webViewTouchListener = View.OnTouchListener { _, _ -> needBlockWebView }
     private val animationHelper = AnimationHelper()
 
     override fun attachBaseContext(newBase: Context) {
@@ -28,11 +31,12 @@ class WebviewActivity : AppCompatActivity(), PageLoadingListener {
         super.onCreate(savedInstanceState)
         this.setFinishOnTouchOutside(true)
         setContentView(R.layout.activity_webview)
+        lawProgress = findViewById(R.id.lawProgress)
+        flContent = findViewById(R.id.flContent)
         CereModule.getInstance(application).run {
             attachBridgeView(webview)
             pageLoadingListener = this@WebviewActivity
         }
-        lawProgress = findViewById(R.id.lawProgress)
     }
 
     override fun onDestroy() {
@@ -48,7 +52,9 @@ class WebviewActivity : AppCompatActivity(), PageLoadingListener {
     }
 
     override fun showLoading() {
+        needBlockWebView = true
         lawProgress?.visibility = View.VISIBLE
+
         animationHelper.startTimingEngineOfFloat(durationMs = 2000) {
             lawProgress?.progress = it
         }
@@ -57,11 +63,13 @@ class WebviewActivity : AppCompatActivity(), PageLoadingListener {
     override fun hideLoading() {
         lawProgress?.visibility = View.GONE
         animationHelper.stopAnimation()
+        needBlockWebView = false
     }
 
     private fun attachBridgeView(wv: WebView) {
         webview = wv
-        findViewById<FrameLayout>(R.id.flContent)?.addView(
+        webview.setOnTouchListener(webViewTouchListener)
+        flContent?.addView(
             webview,
             ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
         )
