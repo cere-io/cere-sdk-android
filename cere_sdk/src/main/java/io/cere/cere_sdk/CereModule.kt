9153aa72871f94af0f51ca9e6ee3b9a9b3d36573
Oleg-Bogatyrov <oleg.bogatyrov@timebook.net>
2022-05-26 14:27:14 +0300

    URL parameters extension

--- cere_sdk/src/main/java/io/cere/cere_sdk/CereModule.kt
+++ cere_sdk/src/main/java/io/cere/cere_sdk/CereModule.kt
@@ -128,7 +128,7 @@ class CereModule(private val context: Context) {
         this.appId = appId
         this.integrationPartnerUserId = integrationPartnerUserId
         this.token = token
-        val url = "${baseUrl}?appId=${appId}&integrationPartnerUserId=${integrationPartnerUserId}&platform=android&version=${version}&env=${env}&token=${token}"
+        val url = "${baseUrl}?appId=${appId}&integrationPartnerUserId=${integrationPartnerUserId}&platform=android&version=${version}&env=${env}&token=${token}&externalUserId=${externalUserId}"
         Log.i(TAG, "load url ${url}")
         this.initStatus = InitStatus.Initialising
         this.webview.loadUrl(url)
